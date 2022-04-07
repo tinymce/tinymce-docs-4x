54a3cfea5bb886496770c55909ea94ac4b3fbe89
Lee Newson <lee.newson@tiny.cloud>
2022-04-08 08:08:12 +1000

    DOC-1640: Swap to using GitHub actions to deploy the site

--- _scripts/deploy-production.sh
+++ _scripts/deploy-production.sh
@@ -4,6 +4,10 @@ mkdir -p ./_site/css
 mkdir -p ./_site/scripts
 mkdir -p ./_site/fonts
 
+if [ -f ./_essentials/robots.txt ]; then
+  rm ./_essentials/robots.txt
+fi
+
 curl -o ./_site/css/common.min.css https://www.tiny.cloud/css/common.min.css -L
 curl -o ./_site/css/docs.min.css https://www.tiny.cloud/css/docs.min.css -L
 curl -o ./_site/scripts/docs.min.js https://www.tiny.cloud/scripts/docs.min.js -L
